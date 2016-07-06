9b48c884d42b9a4076f9ebee7f52ca61821f377d
anikamurarka <murarka.anika97@gmail.com>
2016-07-06 13:21:57 +0530

    Update AlphabetorNot.c

--- AlphabetorNot.c
+++ AlphabetorNot.c
@@ -5,17 +5,14 @@ void main()
 char c;
 printf("Enter a alpha or numeric number");
 scanf("%c",&c);
-if(c>=65 && c<=122)
-{
  if(c>=96 && c<=122)
  {
  printf("The Enter value %c is a lower case alphabet",c);
  }
-  else
+ else if(c>=65 && c<=90)
   {
   printf("The entered value %c is a upper case alphabet",c);
   }
- }
  else
  {
  printf("The entered value %c is not a alphabet",c);
