7adc8c4b57bcdbe04de388960d167e9fb0f89dfa
DeepikaMai <deepikanr29@gmail.com>
2016-07-09 04:32:48 +0530

    Update STOI.C

--- STOI.C
+++ STOI.C
@@ -4,19 +4,14 @@
 #include<string.h>
 int main()
 {
-char ch;
 char num[256];
 int dec=0,i,j,len;
 printf("Enter a string : ");
-//gets(num);
 scanf("%d",&num);
 len = strlen(num);
 for(i=0;i<len;i++)
 dec = dec *10 +( num[i] - '0' );
-
-//ch = atoi(num);
 printf("The value entered is %d.\n",dec);
-
 getch();
 return 0;
 }
