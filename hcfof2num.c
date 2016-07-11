b55e15bd8442082a9cf8bab31afae076aa5211cd
DeepikaMai <deepikanr29@gmail.com>
2016-07-12 04:32:35 +0530

    Create hcfof2num.c

---
+++ hcfof2num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,i,hcf=1,min;
+clrscr();
+printf("Enter two numbers ");
+scanf("%d%d",&n1,&n2);
+  min=(n1<n2) ? n1 : n2;
+   for(i=1;i<min;i++)
+    {
+    if(n1%i==0 && n2%i==0)
+     hcf=i;
+    }
+printf("The hcf is %d " ,hcf);
+getch();
+return 0;
+}
