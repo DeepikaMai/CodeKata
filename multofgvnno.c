33958e21d64ee93d997456d2294a6ba2829a1527
DeepikaMai <deepikanr29@gmail.com>
2016-07-11 05:50:30 +0530

    Create multofgvnno.c

---
+++ multofgvnno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,c;
+printf("Enter any number to print multiplication table : ");
+scanf("%d",&a);
+for(i=0;i<11;i++)
+{
+printf("\n%d  *  %d  = %d\n",a,i,a*i;);
+}
+printf("The required multiplication table is printed");
+getch();
+}
