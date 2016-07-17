65c111eb757ea366b69f49ec6d66cb05b27375f7
DeepikaMai <deepikanr29@gmail.com>
2016-07-17 08:03:57 +0530

    Update numofwords.c

--- numofwords.c
+++ numofwords.c
@@ -4,11 +4,11 @@
 void main()
 {
    char str[100];
-   int i = 0, l = 0, f = 1;
+   int i = 0, f = 1;
    clrscr();
    puts("Enter any string\n");
    gets(str);
-   for(i = 0; i <= l-1; i++)
+   for(i = 0; str[i] != '\0' ; i++)
    {
       if(str[i] == ' ')
       {
